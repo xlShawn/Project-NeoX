20206a3869f364ea2fb593c9f636cfb1783227fc
xlShawn <963419321@qq.com>
2019-05-10 10:48:33 +1200

    new

---
+++ project Neo-X/Assets/Script/Shawn_Ui/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Vector3 velocity;
+    Rigidbody rg;
+
+    void Start()
+    {
+        rg = GetComponent<Rigidbody>();
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint );
+
+    }
+
+    void FixedUpdate()
+    {
+        rg.MovePosition(rg.position + velocity * Time.fixedDeltaTime);
+    }
+}
