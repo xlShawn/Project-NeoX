e1970c0dec61f43f91e12b685906d0988f002e5a
xlShawn <963419321@qq.com>
2019-05-12 23:11:25 +1200

    UI

---
+++ project Neo-X/Assets/Script/Shawn_Ui/UI_Forlder/DoorScript/DoorKey.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorKey : MonoBehaviour
+{
+    public bool inTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                DoorScript.doorKey = true;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (inTrigger)
+        {
+            GUI.Box(new Rect(0, 60, 200, 25), "Press E tp take key");
+        }
+    }
+}
