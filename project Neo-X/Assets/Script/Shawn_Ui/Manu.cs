de09e8e79bcc58a4f4e6a08bd721b1ee187715ca
Guest User <Guest@wg401-b-02-06.mddn.vuw.ac.nz>
2019-05-15 14:20:06 +1200

    fix

---
+++ project Neo-X/Assets/Script/Shawn_Ui/Manu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manu : MonoBehaviour
+{
+    public void StartGame(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
---
+++ project Neo-X/Assets/Script/Shawn_Ui/TimeScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeScript : MonoBehaviour
+{
+    Image timerBar;
+    public float maxTime;
+    float timeLeft;
+    public bool hasStarted; 
+    
+    void Start()
+    {
+
+        timerBar = GetComponent<Image>();
+        timeLeft = maxTime;
+    }
+
+    void Update()
+    {
+
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maxTime;
+        }else
+        {
+            SceneManager.LoadScene(4);
+            Time.timeScale = 0;
+        }
+              
+    }
+
+}
