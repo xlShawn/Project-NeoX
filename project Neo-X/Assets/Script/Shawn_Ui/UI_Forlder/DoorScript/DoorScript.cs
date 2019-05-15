1b0fa59dcc350b1361b2ff605011ab9b163b6245
Guest User <Guest@wg401-b-02-06.mddn.vuw.ac.nz>
2019-05-15 14:48:54 +1200

    Fix some UI function

---
+++ project Neo-X/Assets/Script/Shawn_Ui/UI_Forlder/DoorScript/DoorScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public static bool doorKey;
+    public bool open;
+    public bool close;
+
+    public bool inTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (close)
+            {
+                if (doorKey)
+                {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        open = true;
+                        close = false;
+                    }
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    close = true;
+                    open = false;
+                }
+            }
+        }
+
+        if (inTrigger)
+        {
+            if (open)
+            {
+                var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f),Time.deltaTime *200);
+                transform.rotation = newRot;
+            }
+            else
+            {
+                var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0f, 0f, 0f), Time.deltaTime * 200);
+            }
+        }
+    }
+    private void OnGUI()
+    {
+        if (inTrigger)
+        {
+            if (open)
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "press E to close");
+            }
+            else
+            {
+                if (doorKey)
+                {
+                    GUI.Box(new Rect(0, 0, 200, 25), "Press E to open");
+                }
+                else
+                {
+                    GUI.Box(new Rect(0, 0, 200, 25), "Need a key");
+                }
+                
+            }
+        }
+    }
+}
---
+++ project Neo-X/Assets/Script/Shawn_Ui/UI_Forlder/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public float timeStart;
+    public Text textBox;
+
+    bool timerActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = timeStart.ToString("F2");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerActive)
+        {
+            timeStart += Time.deltaTime;
+            textBox.text = timeStart.ToString("F2");
+        }
+    }
+    public void timerButton()
+    {
+        timerActive = !timerActive;
+    }
+}
