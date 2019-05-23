7a94af1dc831bd088e24539f9fe57e2e15721987
xlShawn <963419321@qq.com>
2019-05-23 16:23:10 +1200

    combine,unfinished

---
+++ project Neo-X/Assets/TPPController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPPController : MonoBehaviour
+{
+    public float Speed;
+    
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
