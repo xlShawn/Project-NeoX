c01de4226b8343ce081c0a5af6aea67727bba7ca
Guest User <Guest@wg401-a-08-10.mddn.vuw.ac.nz>
2019-05-27 12:25:00 +1200

    Added animation code

--- project Neo-X/Assets/Scripts/Cam_PlayerScripts/Scripts/MoveController.cs
+++ project Neo-X/Assets/Scripts/Cam_PlayerScripts/Scripts/MoveController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveController : MonoBehaviour {
+
     public void Move(Vector2 direction) {
         transform.position += transform.forward * direction.x * Time.deltaTime + transform.right * direction.y * Time.deltaTime;
     }
