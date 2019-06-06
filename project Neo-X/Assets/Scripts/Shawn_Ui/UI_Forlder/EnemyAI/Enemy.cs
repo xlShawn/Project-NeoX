8d918fdf6b0efb5eabd260c52d746645f78f9150
xlShawn <963419321@qq.com>
2019-06-07 07:14:04 +1200

    Enemy AI almost Done

---
+++ project Neo-X/Assets/Scripts/Shawn_Ui/UI_Forlder/EnemyAI/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : LivingEntity
+{
+
+    public Transform pathHolder;
+
+
+    private void OnDrawGizmos() // create a waypoint and path line on screen
+    {
+        Vector3 startPosition = pathHolder.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach(Transform waypoint in pathHolder)
+        {
+            Gizmos.DrawSphere(waypoint.position, 0.3f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position; 
+        }
+        Gizmos.DrawLine(previousPosition, startPosition);
+    }
+
+
+    NavMeshAgent pathfinder;
+    Transform target;
+
+    protected override void Start()
+    {
+        
+        base.Start();
+
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(UpdatePath());
+        gameObject.GetComponent<EnemyAiFollowPath>().enabled = false;
+    }
+
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = .25f;
+        
+            while (target != null)
+            {
+                Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+                if (!dead)
+                {
+                    pathfinder.SetDestination(targetPosition);
+
+                }
+                yield return new WaitForSeconds(refreshRate);
+            }
+        
+        
+    }
+}
---
+++ project Neo-X/Assets/Scripts/Shawn_Ui/UI_Forlder/EnemyAI/FieldOfViewEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+
+    void OnSceneGUI()
+    {
+        FieldOfView fow = (FieldOfView)target; // reference for Field of view script
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+        //drawing angle
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in fow.visibleTargets)
+        {
+            Handles.DrawLine(fow.transform.position, visibleTarget.position);
+        }
+    }
+
+}
