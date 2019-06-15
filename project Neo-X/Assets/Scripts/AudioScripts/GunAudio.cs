7d6cc8119a8ad205502aea6d7e30b3bf6ece4329
Guest User <Guest@wg401-i-06-07.mddn.vuw.ac.nz>
2019-06-15 14:27:57 +1200

    Sound plays when player shoots and walks. Ruby key textures added

---
+++ project Neo-X/Assets/Scripts/AudioScripts/GunAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunAudio : MonoBehaviour
+{
+    public AudioClip playerShooting;
+
+    public AudioSource GunAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GunAudioSource.clip = playerShooting;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GunAudioSource.clip = playerShooting;
+            GunAudioSource.Play();
+        }
+
+    }
+}
---
+++ project Neo-X/Assets/Scripts/AudioScripts/PlayerWalkAudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalkAudio : MonoBehaviour
+{
+    public AudioClip playerRun;
+
+    public AudioSource PlayerAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerAudioSource.clip = playerRun;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            PlayerAudioSource.clip = playerRun;
+ 
+            PlayerAudioSource.enabled = true;
+            PlayerAudioSource.loop = true;
+        }
+        else
+        {
+            PlayerAudioSource.enabled = false;
+            PlayerAudioSource.loop = false;
+        }
+
+    }
+}
