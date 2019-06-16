3bf2bc8f78262eb1a1a25cf2a62bdd4e8b9dcae7
Aliz <eiizteayzhy.px@gmail.com>
2019-06-16 19:06:58 +1200

    Added Dialogue Systems
    still need to implement it.

---
+++ project Neo-X/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    //public void TriggerDialogue ()
+    //{
+    //    FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    //}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
+    }
+}
