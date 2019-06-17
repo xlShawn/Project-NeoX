8c2c7b26c9c66ff40656f5f0751d4c5484cad951
Aliz <eiizteayzhy.px@gmail.com>
2019-06-17 16:30:04 +1200

    added cutscene_01
    begining

---
+++ project Neo-X/Assets/Scripts/Dialogue/Cutscene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Cutscene : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while (!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        audioSource.Play();
+    }
+}
