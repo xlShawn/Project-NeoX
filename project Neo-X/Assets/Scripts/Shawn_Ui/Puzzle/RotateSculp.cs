b6b584777d26580b38c642c92fda3c72d9f13104
Guest User <Guest@wg401-a-06-10.mddn.vuw.ac.nz>
2019-06-17 15:27:36 +1200

    puzzle

---
+++ project Neo-X/Assets/Scripts/Shawn_Ui/Puzzle/RotateSculp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSculp : MonoBehaviour
+{
+    public bool rota;
+    private void Update()
+    {
+        if (rota == true)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                transform.Rotate(0f, 0f, 90f);
+            }
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("AAAAAA");
+        if (other.attachedRigidbody)
+        {
+            rota = true;
+        }
+        
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        rota = false;
+        Debug.Log("leaving");
+    }
+}
